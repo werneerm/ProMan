ea85df847f091299f5d3291c5eaa3d9b5b205931
Rudi Szabó <rudi.szabo@codecool.com>
2019-09-19 16:47:09 +0200

    remove _appendToElement method

--- static/js/dom.js
+++ static/js/dom.js
@@ -2,21 +2,6 @@
 import { dataHandler } from "./data_handler.js";
 
 export let dom = {
-    _appendToElement: function (elementToExtend, textToAppend, prepend = false) {
-        // function to append new DOM elements (represented by a string) to an existing DOM element
-        let fakeDiv = document.createElement('div');
-        fakeDiv.innerHTML = textToAppend.trim();
-
-        for (let childNode of fakeDiv.childNodes) {
-            if (prepend) {
-                elementToExtend.prependChild(childNode);
-            } else {
-                elementToExtend.appendChild(childNode);
-            }
-        }
-
-        return elementToExtend.lastChild;
-    },
     init: function () {
         // This function should run once, when the page is loaded.
     },
@@ -44,7 +29,8 @@ export let dom = {
             </ul>
         `;
 
-        this._appendToElement(document.querySelector('#boards'), outerHtml);
+        let boardsContainer = document.querySelector('#boards');
+        boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
     },
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
