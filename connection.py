f26842b00155e85e318509c3c18fb997a30aecd5
werneerm <wmarcos25@gmail.com>
2020-03-05 11:14:02 +0100

    cards going to their places finally

--- connection.py
+++ connection.py
@@ -5,8 +5,8 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = ('adam')
-    password = ('madafaka911')
+    user_name = ('mark')
+    password = ('filekezgya25')
     host = ('localhost')
     database_name =('proman')
 
