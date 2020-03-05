931d1bca152f34c8cfd6068c68d962b19b0e79ff
HaveaKillerDay <petho.adam911@gmail.com>
2020-03-05 15:48:16 +0100

    cards remove works

--- static/js/dom.js
+++ static/js/dom.js
@@ -57,29 +57,10 @@ export let dom = {
             </div>
             `;
             clnPlace.insertAdjacentHTML("beforeend",boardList);
-            // console.log(board[item]);
-//                          boardList += `
-//                 <div class="board-header"><span class="board-title">${board[item].title}</span>
-//                 </div>
-//                 <div class="board-columns" id="${board[item].id}"></div>
-//
-// `;
-//                          // console.log(outerHtml);
-//                          boardsContainer.textContent = '';
-//                          boardsContainer.insertAdjacentHTML("beforeend", boardList);
-//                  }
+           
             dom.loadStatus();
         }
 
-        // const outerHtml = `
-        //     <ul class="board-container">
-        //         ${boardList}
-        //     </ul>
-        // `;
-
-        // let boardsContainer = document.querySelector('#boards');
-        // boardsContainer.textContent = '';
-        // boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
     },
     loadStatus: function () {
         dataHandler.getStatuses(function (statuses) {
@@ -87,30 +68,6 @@ export let dom = {
         })
     },
     showStatus: function (statuses) {
-        // let columnTitle = document.querySelector('.board-columns');
-//         let statusImport = `
-// //             <div class = "board-column">
-// //             <div class = "board-column-title">${statuses[0].title}</div>
-// //             <div class = "board-column-content" id="${statuses[0].id}"></div>
-// //             </div>
-// //             <div class = "board-column">
-// //                <div class = "board-column-title">${statuses[1].title} </div>
-// //                  <div class = "board-column-content" id="${statuses[1].id}"></div>
-// //                </div>
-// //                <div class = "board-column">
-// //                <div class = "board-column-title">${statuses[2].title} </div>
-// //                  <div class = "board-column-content" id="${statuses[2].id}"></div>
-// //                </div>
-// //             <div class = "board-column">
-// //                <div class = "board-column-title">${statuses[3].title} </div>
-// //                  <div class = "board-column-content" id="${statuses[3].id}"></div>
-// //                </div>`
-// // ;
-//
-//         let statusImport = `
-//
-//         `;
-//         columnTitle.insertAdjacentHTML('beforeend',statusImport);
         dom.loadCards(statuses);
     },
     loadCards: function (statuses) {
@@ -139,41 +96,8 @@ export let dom = {
                         <div class="card-title">${card.title}</div>
                         </div>
                         `;
-            card_place.insertAdjacentHTML('beforeend',cardToImport)
+            card_place.insertAdjacentHTML('beforeend',cardToImport);
 
-        // let sections = document.querySelectorAll('section');
-        // let columns = document.querySelectorAll('.board-column')
-        // // console.log(boardID);
-        // let cards_array = [];
-        // let board_array = [];
-        // let cardToReallyImport;
-        // let coordinate;
-        // for (let num = 0; num < sections.length; num++) {
-        //     for (let num_of_cards = 0; num_of_cards < cards.length; num_of_card  s++) {
-        //         if (sections[num].id == cards[num_of_cards].board_id) {
-        //             // console.log(cards[num_of_cards].board_id);
-        //             cards_array.push(cards[num_of_cards]);
-        //         }
-        //     }
-        //     board_array.push(num + 1);
-        // }
-        // // console.log(cards_array);
-        // for (let good_card_num = 0; good_card_num < cards_array.length; good_card_num++) {
-        //     for (let column_of_num = 0; column_of_num < columns.length; column_of_num++) {
-        //         //&& cards_array[good_card_num].board_id == board_array[0]
-        //         if (cards_array[good_card_num].status_id == columns[column_of_num].dataset.status) {
-        //             // console.log("fos");
-        //             let cardToImport = `
-        //                 <div class="card" draggable="true" ondragstart="drag(event)">
-        //                 <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
-        //                 <div class="card-title">${cards_array[good_card_num].title} | ${cards_array[good_card_num].id}</div>
-        //                 </div>
-        //                 `;
-        //             cardToReallyImport = cardToImport;
-        //         }
-        //     }
-        //     coordinate = good_card_num;
-        //     cards_place[cards_array[coordinate].status_id].insertAdjacentHTML("beforeend", cardToReallyImport);
         }
 
         let btn = document.querySelectorAll('.board-add');
@@ -207,36 +131,7 @@ export let dom = {
                 });
             })
         }
-        // console.log(cards_array);
-        // for (let good_card in cards_array) {
-        //     // console.log(good_card);
-        //     for (let column in columns){
-        //     if (good_card.status_id == column.dataset.status) {
-        //         console.log("fos");
-        //         let cardToImport = `
-        //     <div class="card">
-        //     <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
-        //     <div class="card-title">${card.title}</div>
-        //     </div>
-        //     `;
-        //         cards_place[good_card.status_id].insertAdjacentHTML("beforeend", cardToImport);
-        //     }
-        //     }
-        //     }
-        // for (let card of cards) {
-        //     for (let card_place of cards_place) {
-        //         if (card.status_id == card_place.id) {
-        //         let cardToImport = `
-        //         <div class="card">
-        //         <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
-        //         <div class="card-title">${card.title}</div>
-        //         </div>
-        //         `;
-        //            // card_place.insertAdjacentHTML("beforeend", outerHtml);
-        //          // card_place.insertAdjacentHTML("beforeend", cardToImport);
-        //     }
-        //     }
-        // }
+
     },
     doNothing: function () {
         window.location.reload()
